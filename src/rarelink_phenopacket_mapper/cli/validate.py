5894b57ba563e9f83f529f23677c1f5c92401499
frehburg <fil.rehburg@gmail.com>
2024-09-04 11:16:57 +0200

    add validate command

---
+++ src/rarelink_phenopacket_mapper/cli/validate.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+from pathlib import Path
+
+
+def main():
+    """This method launches the validate command"""
+    arg_parser = argparse.ArgumentParser(
+        prog='validate',
+        description='syntactically validates phenopackets using phenopacket-tools'
+    )
+
+    arg_parser.add_argument('-p', '--path', type=str,
+                            help='Path to Phenopackets')
+
+    args = arg_parser.parse_args()
+
+    if args.path:
+        path = Path(args.path)
+    else:
+        path = Path(os.getcwd())
+
+    validate(path)
+
+
+def validate(path: Path):
+    print(f"{path=}")
+    raise NotImplementedError("The function mapping has not been implemented yet")

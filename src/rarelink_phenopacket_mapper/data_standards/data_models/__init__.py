3792a7e1eb17a55cc108f502c007082521caae37
frehburg <fil.rehburg@gmail.com>
2024-09-04 21:45:02 +0200

    created init and erdri cds for datamodel definitons

---
+++ src/rarelink_phenopacket_mapper/data_standards/data_models/__init__.py
@@ -0,0 +1,5 @@
+"""Selection of rare disease specific data models"""
+from .rarelink_datamodel import RARELINK_DATAMODEL
+from .erdri_cds import ERDRI_CDS
+
+__all__ = ["RARELINK_DATAMODEL", "ERDRI_CDS"]

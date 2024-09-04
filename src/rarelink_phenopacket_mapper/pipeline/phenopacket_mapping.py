d824ae4159c5548d2a9ae5f0310620127aba409b
frehburg <fil.rehburg@gmail.com>
2024-09-04 22:06:52 +0200

    updated outdated code

--- src/rarelink_phenopacket_mapper/pipeline/phenopacket_mapping.py
+++ src/rarelink_phenopacket_mapper/pipeline/phenopacket_mapping.py
@@ -1,13 +1,14 @@
-from rarelink_phenopacket_mapper.data_standards import RarelinkData
+from typing import List
+
 from phenopackets import Phenopacket
 
-from rarelink_phenopacket_mapper.data_standards.data_model import DataModel
+from rarelink_phenopacket_mapper.data_standards.data_model import DataModel, DataModelInstance
 
 
 class PhenopacketMapper:
     def __init__(self, datamodel: DataModel):
         self.datamodel = datamodel
 
-    def map(self, rarelink_data: RarelinkData) -> Phenopacket:
+    def map(self, rarelink_data: List[DataModelInstance]) -> Phenopacket:
         # TODO: Implement the mapping logic
         pass

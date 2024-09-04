a5ac17078ac7d63420a80b5e1b97a109a180c282
frehburg <fil.rehburg@gmail.com>
2024-09-04 22:06:42 +0200

    added input and phenop mapper to init

--- src/rarelink_phenopacket_mapper/pipeline/__init__.py
+++ src/rarelink_phenopacket_mapper/pipeline/__init__.py
@@ -1,3 +1,9 @@
 """This module includes the pipeline for mapping rarelink data to phenopackets."""
 
-__all__ = []
+from .input import read_file, read_redcap_api
+from .phenopacket_mapping import PhenopacketMapper
+
+__all__ = [
+    'read_file', 'read_redcap_api',
+    'PhenopacketMapper'
+]

7e1baed43e23963396e90fb0461f7537ca6999ec
frehburg <fil.rehburg@gmail.com>
2024-09-04 21:03:22 +0200

    created stub for phenopacket mapping

---
+++ src/rarelink_phenopacket_mapper/pipeline/phenopacket_mapping.py
@@ -0,0 +1,11 @@
+from rarelink_phenopacket_mapper.data_standards import RarelinkData
+from phenopackets import Phenopacket
+
+
+class PhenoPacketMapper:
+    def __init__(self):
+        # TODO: init
+        pass
+
+    def map(self, rarelink_data: RarelinkData) -> Phenopacket:
+        pass

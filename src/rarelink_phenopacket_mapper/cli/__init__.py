03c89a8febfcf359fc8d78f94d7a794c1ed470cf
frehburg <fil.rehburg@gmail.com>
2024-09-04 15:54:53 +0200

    added module comment to cli submodule

--- src/rarelink_phenopacket_mapper/cli/__init__.py
+++ src/rarelink_phenopacket_mapper/cli/__init__.py
@@ -1,3 +1,4 @@
+"""This module provides the command line interface for the rarelink-phenopacket-mapper package."""
 from .mapping import mapping
 from .quickstart import quickstart
 from .validate import validate

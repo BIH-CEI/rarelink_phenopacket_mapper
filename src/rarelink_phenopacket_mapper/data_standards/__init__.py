ebd8909b71a637abb699205fab9cc4d0f9a6dc57
frehburg <fil.rehburg@gmail.com>
2024-09-04 21:55:50 +0200

    updated inits

--- src/rarelink_phenopacket_mapper/data_standards/__init__.py
+++ src/rarelink_phenopacket_mapper/data_standards/__init__.py
@@ -1,12 +1,12 @@
 """This submodule defines the data standards used in the project."""
 
 from .code import Coding, CodeableConcept
-from .rarelink_data import RarelinkData
+from .data_model import DataModel, DataField, DataModelInstance, DataFieldValue
 from .code_system import CodeSystem, SNOMED_CT, HPO, MONDO, OMIM, ORPHA, LOINC
 
 __all__ = [
     "Coding", "CodeableConcept",
-    "RarelinkData",
+    "DataModel", "DataField", "DataModelInstance", "DataFieldValue",
     "CodeSystem",
     "SNOMED_CT", "HPO", "MONDO", "OMIM", "ORPHA", "LOINC",
 ]
--- src/rarelink_phenopacket_mapper/data_standards/data_models/__init__.py
+++ src/rarelink_phenopacket_mapper/data_standards/data_models/__init__.py
@@ -1,5 +1,5 @@
 """Selection of rare disease specific data models"""
-from .rarelink_datamodel import RARELINK_DATAMODEL
+from .rarelink_datamodel import RARELINK_DATA_MODEL
 from .erdri_cds import ERDRI_CDS
 
-__all__ = ["RARELINK_DATAMODEL", "ERDRI_CDS"]
+__all__ = ["RARELINK_DATA_MODEL", "ERDRI_CDS"]

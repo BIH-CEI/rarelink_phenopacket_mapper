37f5d6e9504e497020879ea242d1470c0d5502ec
frehburg <fil.rehburg@gmail.com>
2024-09-04 22:46:58 +0200

    added validate to init

--- src/rarelink_phenopacket_mapper/pipeline/__init__.py
+++ src/rarelink_phenopacket_mapper/pipeline/__init__.py
@@ -1,8 +1,9 @@
 """This module includes the pipeline for mapping rarelink data to phenopackets."""
 
 from .input import read_file, read_redcap_api, read_phenopackets
-from .output import write
 from .mapping import PhenopacketMapper
+from .output import write
+from .validate import validate, read_validate
 
 __all__ = [
     'read_file', 'read_redcap_api', 'read_phenopackets',

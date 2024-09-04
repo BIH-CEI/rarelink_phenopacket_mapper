28e4c0d131be7a9dab631f792903e7c10bd5313e
frehburg <fil.rehburg@gmail.com>
2024-09-04 22:26:18 +0200

    added functionality to the phenopacket mapper class

--- src/rarelink_phenopacket_mapper/pipeline/phenopacket_mapping.py
+++ src/rarelink_phenopacket_mapper/pipeline/phenopacket_mapping.py
@@ -1,14 +1,22 @@
-from typing import List
+from pathlib import Path
+from typing import List, Union
 
 from phenopackets import Phenopacket
 
 from rarelink_phenopacket_mapper.data_standards.data_model import DataModel, DataModelInstance
+from rarelink_phenopacket_mapper.pipeline import read_file
 
 
 class PhenopacketMapper:
     def __init__(self, datamodel: DataModel):
-        self.datamodel = datamodel
+        self.data_model = datamodel
 
-    def map(self, rarelink_data: List[DataModelInstance]) -> Phenopacket:
+    def load_data(self, path: Union[str, Path]) -> List[DataModelInstance]:
+        return read_file(path=path, data_model=self.data_model)
+
+    def map(self, rarelink_data: List[DataModelInstance]) -> List[Phenopacket]:
         # TODO: Implement the mapping logic
-        pass
+        raise NotImplementedError
+
+    def write(self, phenopackets: List[Phenopacket], output_path: Union[str, Path]) -> bool:
+        raise NotImplementedError

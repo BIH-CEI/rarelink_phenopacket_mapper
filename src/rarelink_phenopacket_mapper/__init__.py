12412adc09ee962c9372846eb1fb73b58dfb1984
frehburg <fil.rehburg@gmail.com>
2024-09-04 12:17:18 +0200

    updated doc of package in init file of package, auto displayed in doc

--- src/rarelink_phenopacket_mapper/__init__.py
+++ src/rarelink_phenopacket_mapper/__init__.py
@@ -1,4 +1,4 @@
-"""An example description for the rarelink_phenopacket_mapper package."""
+"""A package to map data from the RareLink format in REDCap to the GA4GH Phenopacket schema (v2)."""
 
 __version__ = "0.0.1"
 

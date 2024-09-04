89e603af2eab9fd72da33b6a9f7ecfd526f59a89
frehburg <fil.rehburg@gmail.com>
2024-09-04 21:55:00 +0200

    added datafield value and changed some to frozen

--- src/rarelink_phenopacket_mapper/data_standards/data_model.py
+++ src/rarelink_phenopacket_mapper/data_standards/data_model.py
@@ -1,17 +1,11 @@
 from dataclasses import dataclass
-from typing import Union, Literal, List
+from typing import Union, List
 
 from rarelink_phenopacket_mapper.data_standards import CodeSystem
+from rarelink_phenopacket_mapper.data_standards.date import Date
 
 
-@dataclass(slots=True)
-class RarelinkData:
-    """
-    Data class for Rarelink data
-    """
-
-
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class DataField:
     name: str
     section: str
@@ -22,7 +16,19 @@ class DataField:
     ordinal: str = None
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
+class DataFieldValue:
+    field: DataField
+    value: Union[int, float, str, bool, Date, CodeSystem]
+
+
+@dataclass(slots=True, frozen=True)
 class DataModel:
     name: str
     fields: List[DataField]
+
+
+@dataclass(slots=True)
+class DataModelInstance:
+    data_model: DataModel
+    values: List[DataFieldValue]

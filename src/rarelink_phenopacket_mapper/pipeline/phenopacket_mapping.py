0c8eb57ea46b2998339fadcf1ae0e6c357113069
frehburg <fil.rehburg@gmail.com>
2024-09-04 21:47:07 +0200

    created skeleton for mapper

--- src/rarelink_phenopacket_mapper/pipeline/phenopacket_mapping.py
+++ src/rarelink_phenopacket_mapper/pipeline/phenopacket_mapping.py
@@ -1,11 +1,13 @@
 from rarelink_phenopacket_mapper.data_standards import RarelinkData
 from phenopackets import Phenopacket
 
+from rarelink_phenopacket_mapper.data_standards.rarelink_data import DataModel
+
 
 class PhenoPacketMapper:
-    def __init__(self):
-        # TODO: init
-        pass
+    def __init__(self, datamodel: DataModel):
+        self.datamodel = datamodel
 
     def map(self, rarelink_data: RarelinkData) -> Phenopacket:
+        # TODO: Implement the mapping logic
         pass

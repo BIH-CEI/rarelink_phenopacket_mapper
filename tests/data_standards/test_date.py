5ee3ba112c081df1346d3453788843602121c7db
frehburg <fil.rehburg@gmail.com>
2024-09-05 11:57:25 +0200

    removed unnecessary tests and added class for date testing

---
+++ tests/data_standards/test_date.py
@@ -0,0 +1,27 @@
+import pytest
+
+import rarelink_phenopacket_mapper as rlpm
+
+
+@pytest.fixture
+def example_date():
+    return rlpm.data_standards.Date(
+        year=2024,
+        month=9,
+        day=5,
+        hour=11,
+        minute=38,
+        second=19
+    )
+
+
+@pytest.mark.parametrize("fmt", "result", [
+    ("yyyy-mm-dd", "2024-09-05 11:38:19"),
+    ("yyyy-mm", "2024-09"),
+    ("yyyy", "2024"),
+    ("yyyy-mm-dd hh:mm:ss", "2024-09-05 11:38:19"),
+    ("iso", "2024-09-05T11:38:19"),
+    ("iso8601", "2024-09-05T11:38:19")
+])
+def test_date_format(example_date, fmt, result):
+    assert example_date.formatted_string(fmt) == result

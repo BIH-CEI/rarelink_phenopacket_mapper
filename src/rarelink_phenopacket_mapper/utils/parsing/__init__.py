ce529e416ba749d86c8c603c3eef99f6b27f782d
frehburg <fil.rehburg@gmail.com>
2024-09-06 17:09:25 +0200

    bug: missing dot in init

--- src/rarelink_phenopacket_mapper/utils/parsing/__init__.py
+++ src/rarelink_phenopacket_mapper/utils/parsing/__init__.py
@@ -1,7 +1,7 @@
 """This module contains utility functions concerning the parsing of strings to python values"""
 
 from .parse_data_type import parse_data_type
-from parse_ordinal import parse_ordinal
+from .parse_ordinal import parse_ordinal
 
 __all__ = [
     "parse_data_type",

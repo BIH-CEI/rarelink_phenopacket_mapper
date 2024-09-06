89c4b66b687103cd4cdf05428896fd72757493d7
frehburg <fil.rehburg@gmail.com>
2024-09-06 18:27:35 +0200

    added safe loc to init

--- src/rarelink_phenopacket_mapper/utils/__init__.py
+++ src/rarelink_phenopacket_mapper/utils/__init__.py
@@ -1,4 +1,5 @@
 """This submodule contains utility functions that are used throughout the package."""
 from .create_ipynb_in_code import NotebookBuilder
+from .pandas_utils import loc_default
 
-__all__ = ["NotebookBuilder"]
+__all__ = ["NotebookBuilder", "loc_default"]

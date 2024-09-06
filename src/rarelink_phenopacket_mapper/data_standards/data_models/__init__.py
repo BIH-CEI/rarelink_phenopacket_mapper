0d58afbd0e10299908b73143ecbdde6d08c36038
frehburg <fil.rehburg@gmail.com>
2024-09-06 15:31:20 +0200

    still renaming

--- src/rarelink_phenopacket_mapper/data_standards/data_models/__init__.py
+++ src/rarelink_phenopacket_mapper/data_standards/data_models/__init__.py
@@ -1,6 +1,6 @@
 """Selection of rare disease specific data models"""
 from .rarelink_datamodel import RARELINK_DATA_MODEL
 from .erdri_cds import ERDRI_CDS
-from .data_type_string_representation import parse_string_type_representation
+from .data_type_string_representation import parse_type_string_representation
 
-__all__ = ["RARELINK_DATA_MODEL", "ERDRI_CDS", "parse_string_type_representation"]
+__all__ = ["RARELINK_DATA_MODEL", "ERDRI_CDS", "parse_type_string_representation"]

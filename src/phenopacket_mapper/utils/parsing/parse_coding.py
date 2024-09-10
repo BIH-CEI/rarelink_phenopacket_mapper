f225629e657ebb845280e328f57f223e535d6ab2
frehburg <fil.rehburg@gmail.com>
2024-09-10 17:47:08 +0200

    logic for extracting name space prefix and code from string

--- src/phenopacket_mapper/utils/parsing/parse_coding.py
+++ src/phenopacket_mapper/utils/parsing/parse_coding.py
@@ -4,8 +4,6 @@ import re
 from phenopacket_mapper.data_standards import Coding, CodeSystem
 from phenopacket_mapper.utils.parsing import get_codesystem_by_namespace_prefx
 
-from phenopacket_mapper.data_standards import Coding, CodeSystem
-
 
 def parse_coding(
         coding_str: str,
@@ -24,3 +22,23 @@ def parse_coding(
     pattern = r'^(.*?):(.*)$'
 
     match = re.match(pattern, coding_str)
+
+    if match:
+        namespace_prefix = match.group(1)  # Part before the colon
+        code = match.group(2)  # Part after the colon
+
+        code_system = get_codesystem_by_namespace_prefx(namespace_prefix, resources)
+
+        if code_system:
+            return Coding(system=code_system, code=code)
+        else:
+            if compliance == 'hard':
+                raise ValueError(f"Code system with namespace prefix '{namespace_prefix}' not found in resources.")
+            else:
+                print(f"Warning: Code system with namespace prefix '{namespace_prefix}' not found in resources.")
+                coding = Coding(system=namespace_prefix, code=code)
+                print(f"Warning: Returning Coding object with system as namespace prefix and code as '{code}'")
+                return coding
+
+    else:
+        raise ValueError(f"Invalid coding string: {coding_str}")

85a821c7ebf42f41380883e89b4bc46c3b0b8f7d
frehburg <fil.rehburg@gmail.com>
2024-09-10 14:25:23 +0200

    defined some sample value sets

--- src/phenopacket_mapper/data_standards/value_set.py
+++ src/phenopacket_mapper/data_standards/value_set.py
@@ -15,3 +15,15 @@ class ValueSet:
                         elements=list(set(self.elements + value_set.elements)),
                         description=new_description)
 
+
+TRUE_FALSE_VALUE_SET = ValueSet(name="TrueFalseValueSet",
+                                elements=[True, False],
+                                description="A value set for True and False")
+
+UNKNOWN_VALUE_SET = ValueSet(name="UnknownValueSet",
+                             elements=["unknown"],
+                             description="A value set for Unknown")
+
+TRUE_FALSE_UNKNOWN_VALUE_SET = TRUE_FALSE_VALUE_SET.extend(new_name="TrueFalseUnknownValueSet",
+                                                           value_set=UNKNOWN_VALUE_SET,
+                                                           new_description="A value set for True, False, and Unknown")

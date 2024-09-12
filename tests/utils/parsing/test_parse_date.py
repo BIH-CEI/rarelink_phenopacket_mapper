1e5da42a034db1249a64a119bf42d68321ea3563
frehburg <fil.rehburg@gmail.com>
2024-09-12 12:49:33 +0200

    added tests to check some more irregular strings

--- tests/utils/parsing/test_parse_date.py
+++ tests/utils/parsing/test_parse_date.py
@@ -1,5 +1,6 @@
 import pytest
 
+from phenopacket_mapper.data_standards import Date
 from phenopacket_mapper.utils.parsing import parse_date
 
 
@@ -22,4 +23,26 @@ from phenopacket_mapper.utils.parsing import parse_date
 ])
 def test_parse_date_(date_str, expected):
     assert parse_date(date_str) == expected
+
+
+@pytest.mark.parametrize("date_str, expected", [
+    ("2024-1-01", Date(day=1, month=1, year=2024)),
+    ("2024-01-1", Date(day=1, month=1, year=2024)),
+    ("2024-1-1", Date(day=1, month=1, year=2024)),
+    ("1999/2/27", Date(day=27, month=2, year=1999)),
+    ("3/31/1934", Date(day=31, month=3, year=1934)),
+    ("4-13-2013", Date(day=13, month=4, year=2013)),
+    ("27/5/1990", Date(day=27, month=5, year=1990)),
+    ("19.6.2024", Date(day=19, month=6, year=2024)),
+    ("20-7-2024", Date(day=20, month=7, year=2024)),
+    ("2024-8", Date(month=8, year=2024)),
+    ("2024/9", Date(month=9, year=2024)),
+    ("2024.10", Date(month=10, year=2024)),
+    ("11.2024", Date(month=11, year=2024)),
+    ("12-2024", Date(month=12, year=2024)),
+    ("1/2024", Date(month=1, year=2024)),
+    ("2024", Date(year=2024)),
+    ("2024-9-12 12:33:44", Date(second=44, minute=33, hour=12, day=12, month=9, year=2024)),
+])
+def test_parse_date_irregularities(date_str, expected):
     assert parse_date(date_str) == expected

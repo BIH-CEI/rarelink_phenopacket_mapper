34ccc3089860105d3e95604ed21de562feb85286
frehburg <fil.rehburg@gmail.com>
2024-09-12 15:02:03 +0200

    copied code over from value set

---
+++ src/phenopacket_mapper/utils/parsing/parse_value_set.py
@@ -0,0 +1,45 @@
+from typing import List, Literal
+
+from phenopacket_mapper.data_standards import CodeSystem
+from phenopacket_mapper.data_standards.value_set import ValueSet
+from phenopacket_mapper.utils.parsing import parse_single_data_type, parse_value
+
+
+def parse_value_set(
+        value_set_str: str,
+        value_set_name: str,
+        value_set_description: str = "",
+        resources: List[CodeSystem] = None,
+        compliance: Literal['soft', 'hard'] = 'soft',
+) -> 'ValueSet':
+    """Parses a value set from a string representation
+
+    >>> ValueSet.parse_value_set("True, False", "TrueFalseValueSet", "A value set for True and False", [])
+    ValueSet(name="TrueFalseValueSet", elements=[True, False], description="A value set for True and False")
+
+    :param value_set_str: String representation of the value set
+    :param value_set_name: Name of the value set
+    :param value_set_description: Description of the value set
+    :param resources: List of CodeSystems to use for parsing the value set
+    :param compliance: Compliance level for parsing the value set
+    :return: A ValueSet object as defined by the string representation
+    """
+    if resources is None:
+        resources = []
+
+    value_set_str = value_set_str.replace(" ", "")  # Remove spaces
+
+    elements_str = value_set_str.split(",")
+
+    elements = []
+    for element_str in elements_str:
+        # parsing as a data type
+        try:
+            # compliance is set to 'hard' because we want to raise an error if the element is not recognized
+            element = parse_single_data_type(type_str=element_str, resources=resources, compliance='hard')
+        except ValueError:  # parsing as type failed, parsing as a value
+            element = parse_value(value_str=element_str, resources=resources)
+
+        elements.append(element)
+
+        return ValueSet(name=value_set_name, elements=elements, description=value_set_description)

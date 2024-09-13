04451b9a7c0bb0284c35ac805b1360b49ae5a31f
frehburg <fil.rehburg@gmail.com>
2024-09-13 15:59:23 +0200

    copied over read and added to init

--- src/phenopacket_mapper/pipeline/__init__.py
+++ src/phenopacket_mapper/pipeline/__init__.py
@@ -1,12 +1,12 @@
 """This module includes the pipeline for mapping  data to phenopackets."""
 
-from .input import read_file, read_data_model, read_phenopackets
+from .input import read_file, read_data_model, read_phenopackets, read_phenopacket_from_json
 from .mapper import PhenopacketMapper
 from .output import write
 from .validate import validate, read_validate
 
 __all__ = [
-    'read_file', 'read_data_model',
+    'read_file', 'read_data_model', 'read_phenopackets', 'read_phenopacket_from_json',
     'write',
     'PhenopacketMapper'
 ]

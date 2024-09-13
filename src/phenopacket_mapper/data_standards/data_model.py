480162e1c0838dd8f69821293dd97bae8bebbfe1
frehburg <fil.rehburg@gmail.com>
2024-09-13 15:47:57 +0200

    read data model now also in data model class

--- src/phenopacket_mapper/data_standards/data_model.py
+++ src/phenopacket_mapper/data_standards/data_model.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Union, List
+from pathlib import Path
+from types import MappingProxyType
+from typing import Union, List, Literal, Dict
 
 from phenopacket_mapper.data_standards import CodeSystem
 from phenopacket_mapper.data_standards.date import Date
@@ -50,6 +52,51 @@ class DataModel:
             ret += f"\t{str(res)}\n"
         ret += ")"
         return ret
+
+    @staticmethod
+    def from_file(
+            data_model_name: str,
+            resources: List[CodeSystem],
+            path: Union[str, Path],
+            file_type: Literal['csv', 'excel', 'unknown'] = 'unknown',
+            column_names: Dict[str, str] = MappingProxyType({
+                DataField.name.__name__: 'data_field_name',
+                DataField.section.__name__: 'data_model_section',
+                DataField.description.__name__: 'description',
+                DataField.value_set.__name__: 'value_set',
+                DataField.required.__name__: 'required',
+                DataField.specification.__name__: 'specification',
+                DataField.ordinal.__name__: 'ordinal'
+            }),
+            parse_value_sets: bool = False,
+            remove_line_breaks: bool = False,
+            parse_ordinals: bool = True,
+    ) -> 'DataModel':
+        """Reads a Data Model from a file
+
+        :param data_model_name: Name to be given to the `DataModel` object
+        :param resources: List of `CodeSystem` objects to be used as resources in the `DataModel`
+        :param path: Path to Data Model file
+        :param file_type: Type of file to read, either 'csv' or 'excel'
+        :param column_names: A dictionary mapping from each field of the `DataField` (key) class to a column of the file
+                            (value). Leaving a value empty (`''`) will leave the field in the `DataModel` definition empty.
+        :param parse_value_sets: If True, parses the string to a ValueSet object, can later be used to check
+                            validity of the data. Optional, but highly recommended.
+        :param remove_line_breaks: Whether to remove line breaks from string values
+        :param parse_ordinals: Whether to extract the ordinal number from the field name. Warning: this can overwrite values
+                                 Ordinals could look like: "1.1.", "1.", "I.a.", or "ii.", etc.
+        """
+        from phenopacket_mapper.pipeline import read_data_model
+        return read_data_model(
+            data_model_name,
+            resources,
+            path,
+            file_type,
+            column_names,
+            parse_value_sets,
+            remove_line_breaks,
+            parse_ordinals
+        )
 
 
 @dataclass(slots=True)

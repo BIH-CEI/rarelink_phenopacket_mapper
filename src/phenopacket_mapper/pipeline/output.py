820d3462b0f00af5eba1fa5d0ee923c7140c6c32
frehburg <fil.rehburg@gmail.com>
2024-09-13 16:02:03 +0200

    copied over write (already in init)

--- src/phenopacket_mapper/pipeline/output.py
+++ src/phenopacket_mapper/pipeline/output.py
@@ -1,13 +1,40 @@
+import os
 from pathlib import Path
 from typing import List, Union
 
+from google.protobuf.json_format import MessageToJson
 from phenopackets.schema.v2 import Phenopacket
 
 
-def write(phenopackets: List[Phenopacket], output_path: Union[str, Path]):
+def write(
+        phenopackets_list: List[Phenopacket], out_dir: Union[str, Path]
+) -> None:
+    """Writes a list of phenopackets to JSON files.
+
+    :param phenopackets_list: The list of phenopackets.
+    :type phenopackets_list: List[Phenopacket]
+    :param out_dir: The output directory.
+    :type out_dir: Union[str, Path]
     """
-    Write a list of phenopackets to a file
-    :param phenopackets: List of phenopackets
-    :param output_path: the path to write the phenopackets to
+    # Make sure output out_dr exists.
+    os.makedirs(out_dir, exist_ok=True)
+
+    for phenopacket in phenopackets_list:
+        _write_single_phenopacket(phenopacket, out_dir)
+
+
+def _write_single_phenopacket(
+        phenopacket: Phenopacket,
+        out_dr: Union[str, Path]
+) -> None:
+    """Writes a phenopacket to a JSON file.
+
+    :param phenopacket: The phenopacket.
+    :type phenopacket: Phenopacket
+    :param out_dr: The output directory.
+    :type out_dr: Union[str, Path]
     """
-    raise NotImplementedError
+    json_str = MessageToJson(phenopacket)  # Convert phenopacket to JSON string.
+    out_path = os.path.join(out_dr, (phenopacket.id + '.json'))
+    with open(out_path, 'w') as fh:
+        fh.write(json_str)

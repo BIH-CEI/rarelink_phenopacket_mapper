4bad3d91bdc3f0706f4b7650f371e5a42dc2988e
frehburg <fil.rehburg@gmail.com>
2024-09-15 20:37:23 +0200

    added load data using data model to pipeline init

--- src/phenopacket_mapper/pipeline/__init__.py
+++ src/phenopacket_mapper/pipeline/__init__.py
@@ -1,12 +1,12 @@
 """This module includes the pipeline for mapping  data to phenopackets."""
 
-from .input import read_file, read_data_model, read_phenopackets, read_phenopacket_from_json
+from .input import read_file, read_data_model, read_phenopackets, read_phenopacket_from_json, load_data_using_data_model
 from .mapper import PhenopacketMapper
 from .output import write
 from .validate import validate, read_validate
 
 __all__ = [
-    'read_file', 'read_data_model', 'read_phenopackets', 'read_phenopacket_from_json',
+    'read_file', 'read_data_model', 'read_phenopackets', 'read_phenopacket_from_json', 'load_data_using_data_model',
     'write',
     'PhenopacketMapper'
 ]

0ae33f063024f84815bd67e185b6389d83f06924
frehburg <fil.rehburg@gmail.com>
2024-09-18 13:51:00 +0200

    added doc for the id field

--- src/phenopacket_mapper/data_standards/data_model.py
+++ src/phenopacket_mapper/data_standards/data_model.py
@@ -26,8 +26,20 @@ class DataField:
     A dataa field is the equivalent of a column in a table. It has a name, a value set, a description, a section, a
     required flag, a specification, and an ordinal.
 
+    The string for the `id` field is generated from the `name` field using the `str_to_valid_id` function from the
+    `phenopacket_mapper.utils` module. This attempts to convert the `name` field. Sometimes this might not work as
+    desired, in which case the `id` field can be set manually.
+
+    Naming rules for the `id` field:
+    - The `id` field must be a valid Python identifier
+    - The `id` field must start with a letter or the underscore character
+    - The `id` field must cannot start with a number
+    - The `id` field can only contain lowercase alpha-numeric characters and underscores (a-z, 0-9, and _ )
+    - The `id` field cannot be any of the Python keywords (e.g. `in`, `is`, `not`, `class`, etc.).
+
     :ivar name: Name of the field
     :ivar value_set: Value set of the field
+    :ivar id: Id of the field, adhering to the naming rules stated above
     :ivar description: Description of the field
     :ivar section: Section of the field (Only applicable if the data model is divided into sections)
     :ivar required: Required flag of the field
@@ -50,6 +62,7 @@ class DataField:
 
     def __str__(self):
         ret = "DataField(\n"
+        ret += f"\t\tid: {self.id},\n"
         ret += f"\t\tsection: {self.section},\n"
         ret += f"\t\tordinal, name: ({self.ordinal},  {self.name}),\n"
         ret += f"\t\tvalue_set: {self.value_set}, required: {self.required},\n"

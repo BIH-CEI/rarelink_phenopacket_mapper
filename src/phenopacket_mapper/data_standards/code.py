1877960a9d9974410e95403ebb065d81ba917766
frehburg <fil.rehburg@gmail.com>
2024-09-18 11:53:28 +0200

    wrote pydoc for coding
    a

--- src/phenopacket_mapper/data_standards/code.py
+++ src/phenopacket_mapper/data_standards/code.py
@@ -6,8 +6,15 @@ from phenopacket_mapper.data_standards import CodeSystem, code_system
 
 @dataclass(frozen=True, slots=True)
 class Coding:
-    """
-    Data class for Code
+    """Data class for Coding
+
+    A `Coding` is a representation of a concept defined by a code and a code system. It is used in the `CodeableConcept`
+    data class.
+
+    :ivar system: The code system that defines the code
+    :ivar code: The code that represents the concept
+    :ivar display: The human readable representation of the concept
+    :ivar text: A human readable description or other additional text of the concept
     """
     system: Union[str, CodeSystem]
     code: str

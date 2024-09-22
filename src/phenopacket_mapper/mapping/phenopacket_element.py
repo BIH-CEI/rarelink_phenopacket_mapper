76a34b5464fb3a4f1f5e5ea0e79eee040ff072eb
frehburg <fil.rehburg@gmail.com>
2024-09-22 17:01:36 +0200

    wrote method signature of map for phenopacket element

--- src/phenopacket_mapper/mapping/phenopacket_element.py
+++ src/phenopacket_mapper/mapping/phenopacket_element.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Any, List
 
+from phenopacket_mapper.data_standards import DataModelInstance
 from phenopacket_mapper.mapping import MapField
 
 
@@ -13,4 +14,5 @@ class PhenopacketElement:
         for f in self.fields:
             if not hasattr(self.phenopacket_element, f.to_field):
                 raise AttributeError(f"The class: {self.phenopacket_element} has no attribute {f.to_field}")
-            
+
+    def map(self, instance: DataModelInstance):

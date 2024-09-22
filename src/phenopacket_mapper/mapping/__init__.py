0dbad4433f473e8a9d45b69a579eecf8ecc84a41
frehburg <fil.rehburg@gmail.com>
2024-09-22 16:25:50 +0200

    added to init

--- src/phenopacket_mapper/mapping/__init__.py
+++ src/phenopacket_mapper/mapping/__init__.py
@@ -1,8 +1,12 @@
 """This module facilitates the mapping from a local data model to the phenopacket schema"""
 
+from .map_element import MapElement
+from .phenopacket_element import PhenopacketElement
 from .mapper import PhenopacketMapper
 
 __all__ = [
+    'MapElement',
+    'PhenopacketElement',
     'PhenopacketMapper',
 
 ]

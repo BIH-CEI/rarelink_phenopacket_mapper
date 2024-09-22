50d5a001ae1158d747c7b6951e5808d280e3a833
frehburg <fil.rehburg@gmail.com>
2024-09-22 15:58:02 +0200

    moved mapper

--- src/phenopacket_mapper/mapping/__init__.py
+++ src/phenopacket_mapper/mapping/__init__.py
@@ -1,5 +1,8 @@
 """This module facilitates the mapping from a local data model to the phenopacket schema"""
 
+from .mapper import PhenopacketMapper
+
 __all__ = [
+    'PhenopacketMapper',
 
 ]
--- src/phenopacket_mapper/pipeline/__init__.py
+++ src/phenopacket_mapper/pipeline/__init__.py
@@ -1,7 +1,7 @@
 """This module includes the pipeline for mapping  data to phenopackets."""
 
 from .input import read_data_model, read_phenopackets, read_phenopacket_from_json, load_data_using_data_model
-from .mapper import PhenopacketMapper
+from phenopacket_mapper.mapping.mapper import PhenopacketMapper
 from .output import write
 from .validate import validate, read_validate
 

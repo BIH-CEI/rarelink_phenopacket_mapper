b1131ed0db32b80bb49611f91bf96db9d155e55d
frehburg <fil.rehburg@gmail.com>
2024-09-24 20:12:46 +0200

    added kwargs to preprocess

--- src/phenopacket_mapper/preprocessing/preprocess.py
+++ src/phenopacket_mapper/preprocessing/preprocess.py
@@ -4,7 +4,11 @@ from typing import Any, Union, Dict, Callable
 from phenopacket_mapper.preprocessing import preprocess_dict, preprocess_method
 
 
-def preprocess(value: Any, mapping: Union[Dict, Callable]) -> Any:
+def preprocess(
+        value: Any,
+        mapping: Union[Dict, Callable],
+        **kwargs
+) -> Any:
     """Preprocess a value before mapping to a Phenopacket.
 
     Relies on `preprocess_dict` and `preprocess_method` to preprocess using a dictionary or method, respectively. Please
@@ -13,7 +17,7 @@ def preprocess(value: Any, mapping: Union[Dict, Callable]) -> Any:
     if isinstance(mapping, dict):
         return preprocess_dict(value, mapping)
     elif isinstance(mapping, Callable):
-        return preprocess_method(value, mapping)
+        return preprocess_method(value, mapping, **kwargs)
 
     warnings.warn(f"Mapping type {type(mapping)} in preprocessing not supported. Returning original value.")
     return value

1f37c5fe778478f8a2b01195751356f243f561d2
frehburg <fil.rehburg@gmail.com>
2024-10-04 15:20:34 +0200

    addded some doc

--- src/phenopacket_mapper/api_requests/api_request_super_class.py
+++ src/phenopacket_mapper/api_requests/api_request_super_class.py
@@ -4,6 +4,12 @@ from phenopacket_mapper.data_standards import Coding
 
 
 class APIRequestSuperClass(ABC):
+    """Super class for API requests to get details abput concepts from code systems
+
+    A class should implement this super class to provide a method to get details about a concept from one specific
+    code system. An example of this can be seen in `orpha_api_request.py` where the Orphanet API is used to get details
+    about a concept from the Orphanet code system.
+    """
 
     @abstractmethod
     def get(self, concept_id) -> Coding:
--- src/phenopacket_mapper/api_requests/orpha_api_request.py
+++ src/phenopacket_mapper/api_requests/orpha_api_request.py
@@ -7,6 +7,7 @@ from phenopacket_mapper.data_standards import Coding, ORDO
 
 
 class OrphaAPIRequest(APIRequestSuperClass):
+    """A class to request data from the Orphanet API."""
 
     api_base_url = "https://www.orpha.net/en/disease/detail/"
 
@@ -14,6 +15,7 @@ class OrphaAPIRequest(APIRequestSuperClass):
         self.orpha_code_system = orpha_code_system
 
     def get(self, concept_id: int) -> Coding:
+        """Get details about a concept from the Orphanet API."""
         html = rest_get(self.api_base_url + str(concept_id))
 
         soup = BeautifulSoup(html, 'html.parser')

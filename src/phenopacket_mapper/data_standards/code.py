36e4ebb27fdace564f4944459678fe28fd6cf5a1
frehburg <fil.rehburg@gmail.com>
2024-10-04 15:26:43 +0200

    remove repr again for coding

--- src/phenopacket_mapper/data_standards/code.py
+++ src/phenopacket_mapper/data_standards/code.py
@@ -55,9 +55,6 @@ class Coding:
         from phenopacket_mapper.utils.parsing import parse_coding
         return parse_coding(coding_str, resources, compliance)
 
-    def __repr__(self):
-        return f"{self.system.namespace_prefix}:{self.code}"
-
     def __str__(self):
         return f"{self.system.namespace_prefix}:{self.code} ({self.display})"
 

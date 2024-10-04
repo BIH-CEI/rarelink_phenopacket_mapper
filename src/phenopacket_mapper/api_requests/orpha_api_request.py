6dee7789c6ff9c8af2ad574fd39642647e53d301
frehburg <fil.rehburg@gmail.com>
2024-10-04 15:18:07 +0200

    added implementation for get orpha code by concept id

---
+++ src/phenopacket_mapper/api_requests/orpha_api_request.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+from build.lib.phenopacket_mapper.data_standards import CodeSystem
+from phenopacket_mapper.api_requests import APIRequestSuperClass
+from phenopacket_mapper.api_requests import get as rest_get
+from phenopacket_mapper.data_standards import Coding, ORDO
+
+
+class OrphaAPIRequest(APIRequestSuperClass):
+
+    api_base_url = "https://www.orpha.net/en/disease/detail/"
+
+    def __init__(self, orpha_code_system: CodeSystem = ORDO) -> None:
+        self.orpha_code_system = orpha_code_system
+
+    def get(self, concept_id: int) -> Coding:
+        html = rest_get(self.api_base_url + str(concept_id))
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        title = soup.title.string.replace("Orphanet: ", "")
+
+        return Coding(
+            system=self.orpha_code_system,
+            code=str(concept_id),
+            display=title
+        )

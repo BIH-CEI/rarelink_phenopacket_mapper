33c8f36c4cc3c23540ac3b5ab66db4b2fc8f1fc7
frehburg <fil.rehburg@gmail.com>
2024-10-07 12:56:21 +0200

    removed rarelink data model

--- src/phenopacket_mapper/data_standards/data_models/__init__.py
+++ src/phenopacket_mapper/data_standards/data_models/__init__.py
@@ -1,6 +1,5 @@
 """Selection of rare disease specific data models"""
-from .rarelink_datamodel import RARELINK_DATA_MODEL
 from .erdri_cds import ERDRI_CDS
 from phenopacket_mapper.utils.parsing.parse_data_type import parse_data_type
 
-__all__ = ["RARELINK_DATA_MODEL", "ERDRI_CDS", "parse_data_type"]
+__all__ = ["ERDRI_CDS", "parse_data_type"]

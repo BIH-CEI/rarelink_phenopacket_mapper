f6a591d2527a9b966df2797c994902b9dd38bc38
frehburg <fil.rehburg@gmail.com>
2024-10-08 19:07:02 +0200

    added data reader to init

--- src/phenopacket_mapper/utils/io/__init__.py
+++ src/phenopacket_mapper/utils/io/__init__.py
@@ -2,12 +2,14 @@
 
 from .read_json import read_json
 from .read_xml import read_xml
+from .data_reader import DataReader
 from .input import read_data_model, read_phenopackets, read_phenopacket_from_json, load_data_using_data_model
 from .output import write
 
 __all__ = [
     'read_json',
     'read_xml',
+    'DataReader',
     'read_data_model',
     'read_phenopackets',
     'read_phenopacket_from_json',

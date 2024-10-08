15b75eb5beb43ccb57b35b3efb78c5deee8ed28a
frehburg <fil.rehburg@gmail.com>
2024-10-08 12:24:38 +0200

    updated test

--- tests/data_standards/data_models/test_data_field.py
+++ tests/data_standards/data_models/test_data_field.py
@@ -1,7 +1,16 @@
 import pytest
 
+from build.lib.phenopacket_mapper.data_standards import ValueSet
 from phenopacket_mapper.data_standards import DataField
 
+@pytest.fixture
+def name():
+    return "name"
 
-def test_data_field_constructor():
-    assert DataField(name="name", )
+@pytest.mark.parametrize("viable_values, expected", [
+    (str, ValueSet([str])),
+    ([str, int], ValueSet([str, int])),
+    (ValueSet([str, int]), ValueSet([str, int])),
+])
+def test_data_field_constructor(name, viable_values, expected):
+    assert DataField(name=name, viable_values=viable_values).viable_values == expected
